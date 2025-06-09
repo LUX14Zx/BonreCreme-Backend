c45d1c9a2d4fb0542f9d9689c154ac703b47a325
LUX14Zx <lux14zx.dev@gmail.com>
2025-06-09 14:35:21 +0700

    initialized spring boot project

--- src/test/java/com/tlfdt/bonrecreme/BonreCremeApplicationTests.java
+++ src/test/java/com/tlfdt/bonrecreme/BonreCremeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BonreCremeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
