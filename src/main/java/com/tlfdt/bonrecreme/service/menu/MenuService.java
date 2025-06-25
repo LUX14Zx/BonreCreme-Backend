ebf647c8fcf5900d252aca8aebd7890e3c5393d5
LUX14Zx <lux14zx.dev@gmail.com>
2025-06-25 21:33:46 +0700

    Add Menu CRUD

--- src/main/java/com/tlfdt/bonrecreme/service/menu/MenuService.java
+++
@@ -1,7 +0,0 @@
-package com.tlfdt.bonrecreme.service.menu;
-
-import com.tlfdt.bonrecreme.api.v1.customer.dto.MenuResponseDTO;
-
-public interface MenuService {
-    MenuResponseDTO getAllMenuItems();
-}
