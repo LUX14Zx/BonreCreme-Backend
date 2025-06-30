82c6e710ca001bbde55f89b6a520fe9b0552dccc
LUX14Zx <lux14zx.dev@gmail.com>
2025-06-30 07:40:00 +0700

    Add validate in AuthController

--- src/main/java/com/tlfdt/bonrecreme/controller/api/v1/auth/AuthController.java
+++ src/main/java/com/tlfdt/bonrecreme/controller/api/v1/auth/AuthController.java
@@ -7,6 +7,7 @@ import com.tlfdt.bonrecreme.service.auth.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Validated // Enables validation for path variables and request parameters.
 public class AuthController {
 
     private final AuthService authService;
